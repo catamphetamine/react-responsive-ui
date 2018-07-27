118e2b06d46b10d4a19056c27fc49b08bbd1c6aa
catamphetamine <purecatamphetamine@gmail.com>
2018-07-27 11:46:25 +0300

    .

--- samples/ExampleMenuButton.js
+++ samples/ExampleMenuButton.js
@@ -10,33 +10,33 @@ window.ExampleMenuButton = class ExampleComponent extends React.Component
 
 				<Highlight lang="jsx" style={{ marginTop: '40px' }}>{`
 					<PageAndMenu>
-					<SlideOutMenu>
-					<div>Item 1</div>
-					<div>Item 2</div>
-					<div>Item 3</div>
-					</SlideOutMenu>
-					<Page>
-					<MenuButton />
-					Page content
-					</Page>
+						<SlideOutMenu>
+							<div>Item 1</div>
+							<div>Item 2</div>
+							<div>Item 3</div>
+						</SlideOutMenu>
+						<Page>
+							<MenuButton />
+							Page content
+						</Page>
 					</PageAndMenu>
 				`}</Highlight>
 
 				<Highlight lang="css">{`
 					.rrui__slideout-menu {
-					padding-top    : calc(0.3rem * 4);
-					padding-bottom : calc(0.3rem * 4);
+						padding-top    : calc(0.3rem * 4);
+						padding-bottom : calc(0.3rem * 4);
 					}
 
 					.rrui__slideout-menu__item {
-					height        : calc(0.3rem * 7);
-					padding-left  : calc(0.3rem * 5);
-					padding-right : calc(0.3rem * 5);
-					color         : white;
+						height        : calc(0.3rem * 7);
+						padding-left  : calc(0.3rem * 5);
+						padding-right : calc(0.3rem * 5);
+						color         : white;
 					}
 
 					.rrui__slideout-menu__item:active {
-					color : yellow;
+						color : yellow;
 					}
 				`}</Highlight>
 
