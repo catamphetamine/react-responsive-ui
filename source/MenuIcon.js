02f6c262c1a1e4f47c58ecb924875e1635aefc1e
catamphetamine <purecatamphetamine@gmail.com>
2018-09-27 14:51:15 +0300

    Fix <MenuIcon/> transition on initial page load

--- source/MenuIcon.js
+++ source/MenuIcon.js
@@ -2,24 +2,48 @@ import React from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 
-export default function MenuIcon({ expanded, className })
+// `PureComponent` is only available in React >= 15.3.0.
+const PureComponent = React.PureComponent || React.Component
+
+export default class MenuIcon extends PureComponent
 {
-	return (
-		<div className={classNames('rrui__menu-icon', className)}>
-			<div className={classNames('rrui__menu-icon__bar', {
-				// 'rrui__menu-icon__bar--collapsed': !expanded,
-				'rrui__menu-icon__bar--expanded': expanded
-			})}/>
-			<div className={classNames('rrui__menu-icon__bar', {
-				// 'rrui__menu-icon__bar--collapsed': !expanded,
-				'rrui__menu-icon__bar--expanded': expanded
-			})}/>
-			<div className={classNames('rrui__menu-icon__bar', {
-				// 'rrui__menu-icon__bar--collapsed': !expanded,
-				'rrui__menu-icon__bar--expanded': expanded
-			})}/>
-		</div>
-	)
+	state = {}
+
+	// This is to prevent the `transform` animations
+	// of menu icon bars from being played on page load.
+	// (when styles are included on a page via javascript)
+	onClick = () => this.setState({ clicked: true })
+
+	render()
+	{
+		const { expanded, className } = this.props
+		const { clicked } = this.state
+
+		return (
+			<div
+				onClick={this.onClick}
+				className={classNames('rrui__menu-icon', className)}>
+
+				<div className={classNames('rrui__menu-icon__bar', {
+					// 'rrui__menu-icon__bar--collapsed': !expanded,
+					'rrui__menu-icon__bar--transition': clicked,
+					'rrui__menu-icon__bar--expanded': clicked && expanded
+				})}/>
+
+				<div className={classNames('rrui__menu-icon__bar', {
+					// 'rrui__menu-icon__bar--collapsed': !expanded,
+					'rrui__menu-icon__bar--transition': clicked,
+					'rrui__menu-icon__bar--expanded': clicked && expanded
+				})}/>
+
+				<div className={classNames('rrui__menu-icon__bar', {
+					// 'rrui__menu-icon__bar--collapsed': !expanded,
+					'rrui__menu-icon__bar--transition': clicked,
+					'rrui__menu-icon__bar--expanded': clicked && expanded
+				})}/>
+			</div>
+		)
+	}
 }
 
 MenuIcon.propTypes = {
