c77e40128508ba85bdd3861396d9324e60c859d7
catamphetamine <purecatamphetamine@gmail.com>
2018-09-30 00:06:09 +0300

    .

--- samples/ExampleMenuButton.js
+++ samples/ExampleMenuButton.js
@@ -9,38 +9,23 @@ window.ExampleMenuButton = class ExampleComponent extends React.Component
 				<p>
 					<br/>
 					<code className="colored">{'<SlideOutMenu/>'}</code> takes an optional <code className="colored">anchor</code> property which can be one of: <code className="colored">"left"</code>, <code className="colored">"right"</code>, <code className="colored">"top"</code>, <code className="colored">"bottom"</code>.
+					<br/>
+					<br/>
+					<code className="colored">{'<SlideOutMenu/>'}</code> also takes an optional <code className="colored">fullscreen: true/false</code> property.
 				</p>
 
 				<Highlight lang="jsx" style={{ marginTop: '40px' }}>{`
 					<PageAndMenu>
 						<SlideOutMenu anchor="right">
-							<div>Item 1</div>
-							<div>Item 2</div>
-							<div>Item 3</div>
+							<a href="#" className="rrui__slideout-menu__item"> Item 1 </a>
+							<a href="#" className="rrui__slideout-menu__item"> Item 2 </a>
+							<a href="#" className="rrui__slideout-menu__item"> Item 3 </a>
 						</SlideOutMenu>
 						<Page>
 							<MenuButton />
 							Page content
 						</Page>
 					</PageAndMenu>
-				`}</Highlight>
-
-				<Highlight lang="css">{`
-					.rrui__slideout-menu {
-						padding-top    : calc(0.3rem * 4);
-						padding-bottom : calc(0.3rem * 4);
-					}
-
-					.rrui__slideout-menu__item {
-						height        : calc(0.3rem * 7);
-						padding-left  : calc(0.3rem * 5);
-						padding-right : calc(0.3rem * 5);
-						color         : white;
-					}
-
-					.rrui__slideout-menu__item:active {
-						color : yellow;
-					}
 				`}</Highlight>
 			</Example>
 		)
