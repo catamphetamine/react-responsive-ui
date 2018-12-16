1f7c843b2e308acef36426e0fe1ab7ce77df7031
catamphetamine <purecatamphetamine@gmail.com>
2018-12-16 08:53:12 +0300

    TextInputLabel isEmpty refactored

--- source/TextInputLabel.js
+++ source/TextInputLabel.js
@@ -8,44 +8,20 @@ export default function TextInputLabel({ id, value, required, invalid, floats, c
 			htmlFor={ id }
 			className={ classNames('rrui__input-label',
 			{
-				'rrui__input-label--required'          : required && isEmpty(value),
+				'rrui__input-label--required'          : required && isEmptyOrBlank(value),
 				'rrui__input-label--invalid'           : invalid,
 				'rrui__input-label--floating'          : floats,
-				'rrui__text-input__label--placeholder' : floats && isEmpty(value)
+				'rrui__text-input__label--placeholder' : floats && isEmptyOrBlank(value)
 			}) }>
 			{ children }
 		</label>
 	)
 }
 
-// Whether the input is empty
-function isEmpty(value)
-{
-	// `0` is not an empty value
-	if (typeof value === 'number' && value === 0)
-	{
-		return false
+function isEmptyOrBlank(value) {
+	// Whitespace strings are considered empty.
+	if (typeof value === 'string') {
+		return !value || !value.trim()
 	}
-
-	// `false` is not an empty value
-	if (typeof value === 'boolean' && value === false)
-	{
-		return false
-	}
-
-	// An empty string, `undefined`, `null` –
-	// all those are considered an empty value.
-	if (!value)
-	{
-		return true
-	}
-
-	// Whitespace string is also considered empty
-	if (typeof value === 'string' && !value.trim())
-	{
-		return true
-	}
-
-	// Not empty
-	return false
+	return value === undefined || value === null
 }
