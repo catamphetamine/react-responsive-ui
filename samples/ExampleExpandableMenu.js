65dcf94f396071cdcf600c0c086d0150f5ba0662
catamphetamine <purecatamphetamine@gmail.com>
2019-09-18 01:51:49 +0300

    .

--- samples/ExampleExpandableMenu.js
+++ samples/ExampleExpandableMenu.js
@@ -8,13 +8,21 @@ window.ExampleExpandableMenu = class ExampleComponent extends React.Component
 
 	render()
 	{
+		const MenuButton = React.forwardRef((props, ref) => (
+			<Button
+				{...props}
+				ref={ref}
+				className="rrui__button--border">
+				MENU
+			</Button>
+		))
 		return (
 			<Example name="expandable-menu" title="Expandable Menu">
 
 				<ExpandableMenu
 					aria-label="Menu"
 					className="column-width"
-					togglerElement="Menu"
+					button={MenuButton}
 					style={{ ...input_style, marginBottom: 0, width: 'auto' }}>
 					<List.Item onClick={() => alert('Google')}>Google</List.Item>
 					<List.Item onClick={() => alert('Yandex')}>Yandex</List.Item>
@@ -23,7 +31,16 @@ window.ExampleExpandableMenu = class ExampleComponent extends React.Component
 				</ExpandableMenu>
 
 				<Highlight lang="jsx">{`
-					<ExpandableMenu togglerElement="Menu">
+					const MenuButton = React.forwardRef((props, ref) => (
+						<Button
+							{...props}
+							ref={ref}
+							className="rrui__button--border">
+							MENU
+						</Button>
+					))
+
+					<ExpandableMenu button={MenuButton}>
 						<List.Item onClick={() => alert('Google')}>
 							Google
 						</List.Item>
@@ -45,7 +62,7 @@ window.ExampleExpandableMenu = class ExampleComponent extends React.Component
 				<code className="colored">{'<ExpandableMenu/>'}</code> takes properties:
 
 				<ul className="list">
-					<li><code className="colored">togglerElement</code> — a <code className="colored">React.Element</code> that will be wrapped in a <code className="colored">&lt;button/&gt;</code>.</li>
+					<li><code className="colored">button</code> — Menu button component that will receive props: <code className="colored">onClick</code>, <code className="colored">onKeyDown</code>, <code className="colored">onBlur</code>, <code className="colored">aria-expanded</code>, etc. Must use <code className="colored">React.forwardRef()</code> to forward the <code className="colored">ref</code> to the <code className="colored">&lt;button/&gt;</code> DOM Element.</li>
 					<li><code className="colored">togglerButtonProps</code> — (optional) Additional toggler <code className="colored">&lt;button/&gt;</code> properties. Example: <code className="colored">{'{ "title": "Menu" }'}</code>.</li>
 					{/*
 					<li><code className="colored">togglerAriaLabel</code> — (optional) <code className="colored">aria-label</code> for the toggler-wrapping <code className="colored">&lt;button/&gt;</code>.</li>
