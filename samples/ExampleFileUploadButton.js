2e59417b649920b3000826a2ba3b7c5d306d9e85
catamphetamine <purecatamphetamine@gmail.com>
2019-12-06 04:38:56 +0300

    .

--- samples/ExampleFileUploadButton.js
+++ samples/ExampleFileUploadButton.js
@@ -40,6 +40,7 @@ window.ExampleFileUploadButton = class ExampleComponent extends React.Component
 					<li><code className="colored">onChange(file/files)</code> — "On file(s) chosen" handler.</li>
 					<li><code className="colored">multiple</code> — (optional) Pass <code className="colored">true</code> for multi-file upload. Is <code className="colored">false</code> by default.</li>
 					<li><code className="colored">accept</code> — (optional) Can be used to <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#attr-accept" target="_blank">restrict the file MIME-types or extensions</a> available for selection.</li>
+					<li><code className="colored">ext</code> — (optional) If specified, will be transformed into <code className="colored">accept</code> property value. Can be a string or an array of strings. Examples: <code className="colored">"pdf"</code>, <code className="colored">["pdf", "doc", "docx"]</code>.</li>
 				</ul>
 			</Example>
 		)
